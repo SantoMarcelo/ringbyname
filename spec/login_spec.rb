1078b0605c8cedf78480b728b6e3c74006bb2aac
santos.marcelooliveira@outlook.com <santos.marcelooliveira@outlook.com>
2017-12-01 10:34:05 -0200

    reset password

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -10,7 +10,7 @@ describe('Login in application', :inprogress) do
     # mongodb://usuario:senha@servidor:porta/banco_de_dados
   end
 
-  describe('Success login' ) do
+  describe('Success login', :login ) do
     it('login with admin user') do |e|
       
       e.step 'when I access login page' do
@@ -18,18 +18,20 @@ describe('Login in application', :inprogress) do
       end
 
       e.step'and I fill the user and password to login' do
-        login_page.do_login('devqc.user4@ringbyname.com', '123456asd')
+        login_page.do_login('devmarcelo.user1@ringbyname.com', '123456asd')
       end
 
       e.step 'the I can see the home page' do
-        expect(page.title).to eql "RingByName"
+        #wait for load home page
+        home.wait_until_home_menu_visible
+        expect(login_page.current_url).to end_with "/#!/app/welcome-page"
       end
 
       Capybara.current_session.driver.quit
     end
   end
 
-  describe('Login Validations', :pregress) do
+  describe('Login Validations', :loginValidations) do
     it('validate empty user') do |e|
 
       e.step 'when I access login page' do
@@ -53,7 +55,7 @@ describe('Login in application', :inprogress) do
       end
             
       e.step 'when I don´t fill the password and try to login' do
-        login_page.do_login('devqc.user4@ringbyname.com', '')
+        login_page.do_login('devmarcelo.user1@ringbyname.com', '')
       end
       
       e.step 'then I can see the validation message' do
@@ -69,7 +71,7 @@ describe('Login in application', :inprogress) do
       end
             
       e.step 'when I try to login with a incorrect user' do
-        login_page.do_login('devqc.user@ringbyname.com', '123456asd')
+        login_page.do_login('devmarcelo.user@ringbyname.com', '123456asd')
       end
       
       e.step 'then I can see the validation message' do
@@ -85,7 +87,7 @@ describe('Login in application', :inprogress) do
       end
             
       e.step 'when I try to login with incorrect password' do
-        login_page.do_login('devqc.user4@ringbyname.com', '123456')
+        login_page.do_login('devmarcelo.user1@ringbyname.com', '123456')
       end
       
       e.step 'then I can see the validation message' do
@@ -103,8 +105,8 @@ end
 
 describe('Reset Password', :resetPass) do
   user = "devqc.user4@ringbyname.com"
-
-  it('reset password of regular user') do |e|
+  
+  it('success reset password') do |e|
     
     e.step('when I access the reset password page') do
       login_page.load
@@ -119,6 +121,74 @@ describe('Reset Password', :resetPass) do
       expect(reset.message.text).to eql ("× Password reset, email sent to: #{user}")
     end
 
+  end
+
+  describe('Reset password validations') do
+    it('validate empty email') do |e|
+
+      e.step('when I acces th reset password page') do
+        login_page.load
+        login_page.access_reset_page
+      end
+
+      e.step('and dont fill the email field') do
+        reset.do_reset(" ")
+      end
+
+      e.step('the I can see the correct validation message') do
+        expect(reset.message.text).to eql ("× Please enter a valid e-mail address.")
+      end
+    end
+
+    it('validate invalid email') do |e|
+
+      e.step('when I acces th reset password page') do
+        login_page.load
+        login_page.access_reset_page
+      end
+
+      e.step('and I fill invalid email') do
+        reset.do_reset("devmarcelo.user@ringbyname.coim ")
+      end
+
+      e.step('the I can see the correct validation message') do
+        expect(reset.message.text).to eql ("× Sorry, that e-mail address is invalid. Please try again.")
+      end
+    end
+     it('validate incorrect email') do |e|
+
+      e.step('when I acces th reset password page') do
+        login_page.load
+        login_page.access_reset_page
+      end
+
+      e.step('and I fill incorrect email') do
+        reset.do_reset("devmarcelo.user@ringbyname.com ")
+      end
+
+      e.step('the I can see the correct validation message') do
+        expect(reset.message.text).to eql ("× Sorry, that user name cannot be found. Please try again.")
+      end
+    end
+  end
+
+  describe('Cancel password reset') do
+    it('validate cancel password reset') do |e|
+      
+       e.step('when I acces th reset password page') do
+        login_page.load
+        login_page.access_reset_page
+      end
+      
+      e.step('and I click in Cancel link ') do
+        reset.cancel_reset_password
+      end
+
+      e.step('the I can return to login page') do
+        expect(login_page).to be_displayed
+        expect(login_page.current_url).to end_with "/#!/auth/login"
+      end
+    end
   end
 end
 
--- spec/pages.rb
+++ spec/pages.rb
@@ -3,5 +3,6 @@ module Pages
 
     def login_page; Login.new end
     def reset; Reset_Password.new end
+    def home; Home.new end
 
 end
