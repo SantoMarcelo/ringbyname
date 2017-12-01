1c159b4824f2d23cc96b2fc205dd2f9604ed20ca
santos.marcelooliveira@outlook.com <santos.marcelooliveira@outlook.com>
2017-12-01 17:04:08 -0200

    admin

--- spec/pages/admin/setup/users.rb
+++ spec/pages/admin/setup/users.rb
@@ -2,6 +2,15 @@
 class User < SitePrism::Page
 
     element :txt_search, '.input'
+    element :btn_search, '.button'
     element :grid_user_list, '.flex-table'
+    element :select_number_pages, 'select[class^="option.value"]'
+    element :info_total_records, 'strong[class="ng-binding"]'
+    element :link_outbound_caller, 'a[ui-sref="admin.setup.user.outbound"]'
+    element :link_multiple_password, 'a[ui-sref="admin.setup.user.password"]'
+    element :tool_tip_search, 'i[tag="ADMIN_TOOLTIP_00069"]'
+    element :tool_tip_users_extension, 'i[tag="ADMIN_TOOLTIP_00070"]'
+
+
 
 end
