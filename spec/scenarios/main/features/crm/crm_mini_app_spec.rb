8b3bbf21cf08a8c4de945bff74eb7896e62982e5
Marcelo <marcelo.santos@inphonex.com>
2018-11-12 17:10:36 -0200

    crm miniapp

--- spec/scenarios/main/features/crm/crm_mini_app_spec.rb
+++ spec/scenarios/main/features/crm/crm_mini_app_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../../../pages/login/reset_password'
 require_relative '../../../../pages/main/contacts/contact'
 require_relative '../../../../pages/admin/dashboard/dashboard'
 #miniapp_crm_geral - :opportunity_general 
-describe('CRM - Opportunity', :miniapp_crm_geral  ) do
+describe('CRM - Opportunity', :master  ) do
   before do
     # if users.get_number_of_crm_licenses == 0
     #   provisioning('account','crm_licenses', '1')
@@ -217,7 +217,7 @@ describe('CRM - Opportunity', :miniapp_crm_geral  ) do
       contact_owner: admin_user[:full_name]
     }
   end
-  describe('search opportunit in mini app', :master) do
+  describe('search opportunit in mini app', :search_opportunity) do
     before do
       admin_user = get_admin()
       crm_mini_app.insert_opportunity(admin_user, 9)
